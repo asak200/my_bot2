5cd210f92630563d2a4300ab204513a361878e1a
asak200 <abdoorahmansk2004@gmail.com>
2024-08-30 08:18:26 +0300

    dasv

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,8 +1,12 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_path = os.path.join(get_package_share_directory('my_bot2'))
+
     return LaunchDescription([
         Node(
             name='rplidar_composition',
@@ -17,5 +21,14 @@ def generate_launch_description():
                 'inverted': False,
                 'angle_compensate': True,
             }],
+        ),
+
+        Node(
+            package='laser_filters',
+            executable='scan_to_scan_filter_chain',
+            name='scan_filter',
+            output='screen',
+            parameters=[{'filter_chain': 'scan_filter_chain'},
+                        os.path.join(pkg_path,'description','laser_limit.yaml')]
         ),
     ])
